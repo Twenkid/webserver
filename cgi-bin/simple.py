0d46a2933bf3bcbefcd4fdf30fdc895ba794dd59
Todor Arnaudov <twenkid@gmail.com>
2025-04-11 05:17:17 +0300

    Update simple.py
    Print was updated for Python 3.

--- cgi-bin/simple.py
+++ cgi-bin/simple.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
-
+# Created by Petko Minkov (pminkov). Updated for Python 3 by Todor Arnaudov (Twenkid) at 11.4.2025
 import cgi
 import cgitb
 cgitb.enable()
 
 input_data = cgi.FieldStorage()
 
-print 'Content-Type:text/html' #HTML is following
-print                          #Leave a blank line
-print '<h1>Addition Results</h1>'
+print('Content-Type:text/html') #HTML is following
+print()                         #Leave a blank line
+print('<h1>Addition Results</h1>')
 try:
   num1=int(input_data["num1"].value)
   num2=int(input_data["num2"].value)
 except:
-  print '<p>Sorry, we cannot turn your inputs into numbers (integers).</p>'
+  print('<p>Sorry, we cannot turn your inputs into numbers (integers).</p>')
   exit(0)
 
 sum = num1 + num2
-print '<p>{0} + {1} = {2}</p>'.format(num1, num2, sum)
+print('<p>{0} + {1} = {2}</p>'.format(num1, num2, sum))
